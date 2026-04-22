fbf7a90d1cd072c6fd507c28957e21ab0193e0a2
Sajannn-064 <adhikarisajan012@gmail.com>
2026-04-22 20:53:17 +0545

    first day

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function App(){
+   const [count, setcount] = useState (0);
+   function handleClick(){
+    setcount(count +1);
+  }
+  return(  
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem'}}>
+     
+    <h1> Counters are updated simultaneously</h1>
+    <MyButton count={count} onClick={handleClick}/>
+    <MyButton count={count} onClick={handleClick}/>
+    </div>    
+  );
+}
+
+
+function MyButton({count, onClick}){
+  return(
+    <button onClick = {onClick}>
+      Clicked {count} times
+      </button>
+  );
+}
